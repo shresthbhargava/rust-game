d9dfe041443b736ba14121139c9caa29ae4b59cb
shresthbhargava <34016366-shresthbhargava@users.noreply.replit.com>
2025-08-17 10:02:50 +0000

    Add .gitignore and remove build artifacts

--- game/target/debug/deps/game-9b7c687dcf417371.d
+++
@@ -1,5 +0,0 @@
-/home/runner/workspace/game/target/debug/deps/game-9b7c687dcf417371.d: src/main.rs
-
-/home/runner/workspace/game/target/debug/deps/libgame-9b7c687dcf417371.rmeta: src/main.rs
-
-src/main.rs:
--- game/target/debug/deps/game-a5260dd5354f0dfa.d
+++
@@ -1,5 +0,0 @@
-/home/runner/workspace/game/target/debug/deps/game-a5260dd5354f0dfa.d: src/main.rs
-
-/home/runner/workspace/game/target/debug/deps/libgame-a5260dd5354f0dfa.rmeta: src/main.rs
-
-src/main.rs:
