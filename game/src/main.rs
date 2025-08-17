6cea7735e0c973007bed521ac7a7a2e6f3098578
shresthbhargava <34016366-shresthbhargava@users.noreply.replit.com>
2025-08-17 10:40:59 +0000

    Fix compilation errors and update code

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt;
+enum Gem {
+    Diamond,
+    Ruby,
+    Sapphire,
+    Topaz,
+    Onyx,
+    Jade,
 }
+impl fmt::Display for Gem {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+    write!(f,"test")
+  }
+    
+}
+fn main(){
+  let gems=[
+    (Gem::Onyx,25.00),
+    (Gem::Ruby,10.00),
+    (Gem::Sapphire,15.00),
+    (Gem::Topaz,10.00),
+  ];
+  for (gem,price) in gems{
+    println!("{}:${}",gem,price);
+  }}
